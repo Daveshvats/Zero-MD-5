5cb4bae0b8f633e4be7b4c5865eb795a1613d86b
Daveshvats <vatsdavesh@gmail.com>
2023-07-14 15:34:52 +0530

    n9gewr8nygfad uaoigbcADWS

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,6 +1,28 @@
 import fs from "fs"
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   async function writeExifImg (media, metadata) {
+    async function imageToWebp (media) {
+      const tmpFileOut = path.join(tmpdir(), `${crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`)
+      const tmpFileIn = path.join(tmpdir(), `${crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.jpg`)
+      fs.writeFileSync(tmpFileIn, media)
+      await new Promise((resolve, reject) => {
+          ffmpeg(tmpFileIn)
+              .on("error", reject)
+              .on("end", () => resolve(true))
+              .addOutputOptions([
+                  "-vcodec",
+                  "libwebp",
+                  "-vf",
+                  "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse"
+              ])
+              .toFormat("webp")
+              .save(tmpFileOut)
+      })
+      const buff = fs.readFileSync(tmpFileOut)
+      fs.unlinkSync(tmpFileOut)
+      fs.unlinkSync(tmpFileIn)
+      return buff
+  }
     let wMedia = await imageToWebp(media)
     const tmpFileIn = path.join(tmpdir(), `${crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`)
     const tmpFileOut = path.join(tmpdir(), `${crypto.randomBytes(6).readUIntLE(0, 6).toString(36)}.webp`)
