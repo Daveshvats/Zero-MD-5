462790fc758308309e87be1915acbe6a6cfa5ecf
Daveshvats <vatsdavesh@gmail.com>
2023-07-24 16:08:19 +0530

    oihktugyhjtfghcrtfg

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -23,7 +23,7 @@ try {
     exp: xp,
     avatar: pp,
     level: user.level,
-    ranklog: 'https://i.ibb.co/WVXgPrB/Black-Simple-Clothing-Brand-Logo-1.png',
+    ranklog: 'https://i.ibb.co/bP0pwS3/fasf-rw.png',
     background: 'https://i.ibb.co/pJFbNTD/back.jpg'
 }, 'apikey')
 
