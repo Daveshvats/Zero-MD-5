5e255e0161bb13953d8041938e18475f9b96a9a4
Daveshvats <vatsdavesh@gmail.com>
2023-07-24 15:49:58 +0530

    hjxczbkholZ

--- plugins/ai-ai.js
+++ plugins/ai-ai.js
@@ -1,8 +1,9 @@
 let handler = async (m, { conn ,text }) => {
-    if (!text) return m.reply ('*Please provide a query*')          
-    let tioress22 = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=BrunoSobrino_2&text=${text}&user=user-unique-id`)
+    if (!text) return m.reply ('*Please provide a query*')  
+    let user = global.DATABASE.data.users[m.sender]        
+    let tioress22 = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=BrunoSobrino_2&text=${text}&user=${user}`)
     let hasill22 = await tioress22.json()
-    conn.sendMessage(m.chat, { text: `${hasill22.message}`.trim() }, { quoted: m });}
+    conn.sendMessage(m.chat, { text: `${hasill22.result}`.trim() }, { quoted: m });}
     handler.help = ['zero']
     handler.tags = ['ai']
     handler.command = ['zero']
