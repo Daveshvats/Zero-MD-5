c8c1212baf9b5573f20a9df7b11b07a3b93703e7
Daveshvats <vatsdavesh@gmail.com>
2023-07-25 15:28:40 +0530

    sdsgfdbv

--- plugins/dl-igimg.js
+++ plugins/dl-igimg.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
     if (!args[0]) throw `✳️Command Usage\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
-   let zero1 = `https://api.itsrose.life/downloader/ig?url=${args[0]}&apikey=Rs-edgarsan`
+   let zero1 = await fetch(`https://api.itsrose.life/downloader/ig?url=${args[0]}&apikey=Rs-edgarsan`)
    let zeroh = await zero1.json()
    conn.sendMessage(m.chat,{image:{url:zeroh.result.url}}, m)
     m.react(done)
