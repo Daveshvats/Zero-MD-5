41c40671555d46f767ca8fc085deb560361559ee
Daveshvats <vatsdavesh@gmail.com>
2023-07-25 15:40:44 +0530

    iujhngbfvdcxsfdsazs

--- plugins/dl-igimg.js
+++ plugins/dl-igimg.js
@@ -1,15 +1,17 @@
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
     if (!args[0]) throw `✳️Command Usage\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
-   let zero1 = await fetch(`https://api.itsrose.life/downloader/ig?url=${args[0]}&apikey=Rs-edgarsan`)
-   let zeroh = await zero1.json()
-   let zero = await fetch(zeroh.result.url)
-   conn.sendMessage(m.chat,{image:{url:zero}}, m)
+    let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+    let json = await human.json()
+    let videoig = json.result
+    let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+    let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+    await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)  
     m.react(done)
 }
-handler.help = ['igimg <link ig>']
+handler.help = ['igvid <link ig>']
 handler.tags = ['dl']
-handler.command = ['ig','igimg'] 
+handler.command = ['ig','igvid'] 
 handler.diamond = true
 
 export default handler 
