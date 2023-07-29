de14601b9b282b87028a478765dba80b525d0d75
Daveshvats <vatsdavesh@gmail.com>
2023-07-29 18:17:01 +0530

    obibdoand

--- plugins/maker-donaldtrump.js
+++ plugins/maker-donaldtrump.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args ,text}) => {
    let response = args.join(' ').split('|')
-  if (!args[0]) throw 'NEEDS TEXT'
+  if (!text) throw 'NEEDS TEXT'
   m.reply('PROCESSING...')
-  let res = `https://api.lolhuman.xyz/api/tweettrump?apikey=BrunoSobrino_2&text=${args[0]}`
+  let res = `https://api.lolhuman.xyz/api/tweettrump?apikey=BrunoSobrino_2&text=${text}`
   conn.sendFile(m.chat, res, 'xynz.jpg', `Its Done`, m, false)
 }
 handler.help = ['donaldtrumptweet'].map(v => v + ' <text>')
