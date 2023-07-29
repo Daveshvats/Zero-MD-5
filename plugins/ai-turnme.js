24ddcd32d560ef2444024a0bfdbc2ef4f0a746ca
Daveshvats <vatsdavesh@gmail.com>
2023-07-29 17:39:06 +0530

    confusioninturnem

--- plugins/ai-turnme.js
+++ plugins/ai-turnme.js
@@ -24,41 +24,44 @@ let handler = async (m, { conn,text }) => {
     let data2 = await q.download()
     let image = await uploadImage(data2)
    const styles = [`${text}`];
-   const baseURL = 'https://api.itsrose.life/image/turnMe'
-  const apikey = 'Rs-edgarsan'
+   for (const [index, style] of styles.entries()) {
+    const { data, status: statusCode } = await axios
+			.request({
+				baseURL: "https://api.itsrose.life", // "https://api.itsrose.site"
+				url: "/image/turnMe",
+				method: "POST",
+				params: {
+					apikey: "Rs-edgarsan",
+				},
+                data :{
+                    'init_image': `${image}`,
+                    "style": `${text}`,
+                    "image_num": 1,
+                    "width": 648,
+                    "height": 864
+                }
+			})
+			.catch((e) => e?.response);
+		const { status, result, message, metadata } = data;
 
-  const config = {
-    headers: {
-      'accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    data: {
-      'init_image': `${image}`,
-      "style": `${text}`,
-      "image_num": 1,
-      "width": 648,
-      "height": 864
-    },
-    params: {
-      apikey:'Rs-edgarsan'
-    }
-  }
-
-  try {
-    const resd = await axios.post(baseURL, config);
-    console.log(resd);
-    const messd = `style: ${resd.metadata.style}`
+		if (!status) {
+			await conn.sendMessage(
+				m.chat,
+				{
+					text: "Generating Stop",
+				},
+				{ quoted: m }
+			);
+			break;
+		}
+		const messd = `style: ${metadata.style}`
 		// Send the base64 image to conn.
 		await conn.sendMessage(
 			m.chat,
-			{ image:{url:`${resd.result.images}`},caption:`${messd}`},
+			{ image:{url:`${result.images}`},caption:`${messd}`},
 			{ quoted: m }
 		);
-  } catch (error) {
-    console.log(error);
-  }
-		
-        }
+        }}
     handler.help = ['turnme']
     handler.tags = ['ai']
     handler.command = ['turnme']
