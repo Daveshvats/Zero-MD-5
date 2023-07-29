72946aa4d693dad64e009445dbd82248949042b3
Daveshvats <vatsdavesh@gmail.com>
2023-07-29 18:28:54 +0530

    gbibdsiujbdiuobj

--- plugins/maker-gta.js
+++ plugins/maker-gta.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'NEEDS TEXT'
   m.reply('PROCESSING...')
-  let res = `https://api.lolhuman.xyz/api/gtapassed?apikey=BrunoSobrino_2&text1=${response[0]}&text2=${response[1]}&apikey=Xynoz`
+  let res = `https://api.lolhuman.xyz/api/gtapassed?apikey=BrunoSobrino_2&text1=${response[0]}&text2=${response[1]}`
   conn.sendFile(m.chat, res, 'xynz.jpg', `Its Done`, m, false)
 }
 handler.help = ['gta'].map(v => v + ' <text>')
