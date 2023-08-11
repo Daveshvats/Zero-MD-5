463802417c10bd12a644e3fc12f2ce3d90902f0e
daveshvats <vatsdavesh@gmail.com>
2023-08-11 22:31:37 +0530

    banchat update

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+let handler = async (m, { conn, isOwner, isAdmin, isROwner , isMods}) => {
+    if (!(isAdmin || isOwner || isMods)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
     m.reply('✅ The bot was deactivated in this group')
 }
